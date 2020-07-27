7ea35e7043ca90b9e0b701ded8f2d74583d34b59
Philip Warren <philwarren1@gmail.com>
2020-07-27 15:50:34 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,8 @@
-
+def my_collect(array)
+   while 
+   yield
+ end
+end
 
 def hamburger(toppings)
   toppings.collect do |topping|
@@ -6,3 +10,17 @@ def hamburger(toppings)
   end
 end
 
+def hello_t(array)
+  if block_given?
+    i = 0
+ 
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+ 
+    array
+  else
+    puts "Hey! No block was given!"
+  end
+end
