9125ac89b55458c33d44e93f8a6f2752c5a17065
Philip Warren <philwarren1@gmail.com>
2020-07-27 15:53:48 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,26 +1,10 @@
 def my_collect(array)
-   while 
-   yield
- end
-end
-
-def hamburger(toppings)
-  toppings.collect do |topping|
-    "I love #{topping} on my burgers"
-  end
-end
-
-def hello_t(array)
-  if block_given?
-    i = 0
- 
-    while i < array.length
+   i = 0
+   while i < array.length
       yield(array[i])
       i = i + 1
     end
- 
     array
-  else
-    puts "Hey! No block was given!"
-  end
+ end
 end
+
